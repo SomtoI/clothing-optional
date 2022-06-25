74ee9473f22819ee235537b2196e617033ed2b2b
SomtoI <somtoidigo@gmail.com>
2022-06-25 23:15:50 +0100

    first re-draft commit

---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  });
+
+//console.log(module);
