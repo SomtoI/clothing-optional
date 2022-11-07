f96652b14e733638d790a7d7812a1a6b776c9d86
SomtoI <somtoidigo@gmail.com>
2022-11-07 10:51:24 +0100

    Added route setup

--- Javascript/clothing-optional/src/components/directory/directory.component.jsx
+++ Javascript/clothing-optional/src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import MenuItem from '../menu-item/menu-item.component';
-import './directory.styles.scss';
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
 
 class Directory extends React.Component {
   constructor() {
@@ -42,21 +42,17 @@ class Directory extends React.Component {
         },
       ],
     };
-
   }
 
-  render(){
+  render() {
     return (
-        <div className="directory-menu">
-            {
-                this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key = {id} title = {title} imageUrl = {imageUrl} size = {size}  />
-                ))
-            }
-        </div>
-    )
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, ...sectionProps }) => (
+          <MenuItem key={id} {...sectionProps} />
+        ))}
+      </div>
+    );
   }
-
 }
 
 export default Directory;
